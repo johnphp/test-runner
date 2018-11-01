7f1fb4fc6365eb5ac4c13df704e11eed862f30c2
john <phptr@yahoo.com>
2018-10-31 21:53:54 -0700

    first commit

---
+++ test_dua.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestDua(unittest.TestCase):
+    
+    def test_odd(self):
+        self.assertEqual(1+2, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
---
+++ test_satu.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestSatu(unittest.TestCase):
+    
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
